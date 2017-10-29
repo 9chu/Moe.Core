bbe0fc52b98c068ff63cdf415156e67cf20fdccf
chu <1871361697@qq.com>
2017-10-29 21:56:22 +0800

    👍 Fix bug

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -23,6 +23,8 @@ std::string& moe::ReadWholeFileInPlace(std::string& out, const char* path)
         MOE_THROW(IOException, "Seek to end on file \"{0}\" error", path);
 
     auto size = t.tellg();
+    if (size < 0)
+        MOE_THROW(IOException, "Tellg on file \"{0}\" error", path);
     out.reserve(size);
 
     t.seekg(0, std::ios::beg);
@@ -30,7 +32,7 @@ std::string& moe::ReadWholeFileInPlace(std::string& out, const char* path)
         MOE_THROW(IOException, "Seek to begin on file \"{0}\" error", path);
 
     out.assign((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
-    if (out.length() != size)
+    if (out.length() != (size_t)size)
         MOE_THROW(IOException, "Read file \"{0}\" error", path);
     return out;
 }
